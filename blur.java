8b8a4fa735e9ca6a0edfb44a56fbed6d339553d9
yuliak8 <yuliak8@my.yorku.ca>
2025-03-26 22:46:30 -0400

    Add files via upload

---
+++ blur.java
@@ -0,0 +1,53 @@
+
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+
+
+public class blur {
+	
+	public void convert(String inputFileName, String outputFileName) throws IOException {
+		
+		BufferedImage inputImage = ImageIO.read(new File(inputFileName));
+		int width = inputImage.getWidth();
+		int height = inputImage.getHeight();
+		
+		BufferedImage outputImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		
+		int blurRadius = 2;
+		
+		for(int y = 0; y<height; y++) {
+			for(int x=0; x<width; x++) {
+				int totalRed = 0;
+				int totalGreen = 0;
+				int totalBlue = 0;
+				int count = 0;
+				
+				for(int dy = -blurRadius; dy <= blurRadius; dy++) {
+					for(int dx = -blurRadius;dx<=blurRadius;dx++) {
+						int nx = x + dx;
+						int ny = y + dy;
+						
+						if(nx >= 0 && nx < width && ny>=0 && ny<height) {
+							ARGB pixel = new ARGB(inputImage.getRGB(nx,  ny));
+							totalRed += pixel.red;
+							totalGreen += pixel.green;
+							totalBlue += pixel.blue;
+							count++;
+						}
+					}
+				}
+				
+				ARGB blurredPixel = new ARGB(255, totalRed/count, totalGreen/count, totalBlue/count);
+				
+				outputImage.setRGB(x, y, blurredPixel.toInt());
+			}
+		}
+		
+		ImageIO.write(outputImage, "png",  new File(outputFileName));
+	}
+	
+
+}

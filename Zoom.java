3657e5dfa338b019c36c249f154bce71c27ab21a
yuliak8 <yuliak8@my.yorku.ca>
2025-03-30 12:58:58 -0400

    Update and rename zoom.java to Zoom.java

--- Zoom.java
+++ Zoom.java
@@ -4,7 +4,7 @@ import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 
-public class zoom extends Converter{
+public class Zoom extends Converter{
 	
 	@Override
 	public void convert(String inputFileName, String outputFileName) throws IOException {

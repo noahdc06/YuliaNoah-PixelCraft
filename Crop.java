cd100c856adc824c653b11cfbbeb8f750061b1c5
noahdc06 <noahdc06@my.yorku.ca>
2025-03-30 15:56:53 -0400

    Update Crop.java

--- Crop.java
+++ Crop.java
@@ -4,7 +4,7 @@ import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 
-public class Crop extends Converter {
+public class Crop extends BaseConverter {
 
 	@Override
 	public void convert(String inputFileName, String outputFileName) throws IOException {

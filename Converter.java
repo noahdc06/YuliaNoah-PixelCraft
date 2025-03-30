c3a044afafc245d6a77ac0b80a1de83c184700ff
noahdc06 <noahdc06@my.yorku.ca>
2025-03-30 13:15:55 -0400

    Update Converter.java

--- Converter.java
+++ Converter.java
@@ -1,8 +1,12 @@
 import java.io.IOException;
-
+/* File's initial purpose was to be implemented in every converter to allow PixelCraft
+ * to run without adding if statements for each converter, which would break the
+ * requirement that the PixelCraft main file not be changed.*/
 
 public interface Converter {
-/*We initially had this interface implemented in EVERY converter, then later realized
- **/
+/* Once we tried using inheritance, we only needed to implement this into the
+ * BaseConverter file, as that abstract class does all the file management, and
+ * we can connect to each converter through extending BaseConverter to them
+ * individually.*/
     void convert(String inputFileName, String outputFileName) throws IOException;
 }

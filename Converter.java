439491075dc150ef4bc09b738e6c68e0fbb62972
noahdc06 <noahdc06@my.yorku.ca>
2025-03-30 13:12:11 -0400

    Update Converter.java

--- Converter.java
+++ Converter.java
@@ -2,8 +2,7 @@ import java.io.IOException;
 
 
 public interface Converter {
-/*This file is implemented in all converters and allows PixelCraft to run without 
- * adding if statements for each converter, something which would break the requirement 
- * that the PixelCraft main file not be changed. */
+/*We initially had this interface implemented in EVERY converter, then later realized
+ **/
     void convert(String inputFileName, String outputFileName) throws IOException;
 }

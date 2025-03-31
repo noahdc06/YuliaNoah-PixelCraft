133dc3a4003fa4a6d2c95fffc1ff1e9e4a93abe6
noahdc06 <noahdc06@my.yorku.ca>
2025-03-30 22:56:11 -0400

    Update Rotate.java

--- Rotate.java
+++ Rotate.java
@@ -4,23 +4,25 @@ import java.io.File;
 import java.io.IOException;
 
 public class Rotate extends BaseConverter {
-    @Override
     public void convert(String inputFileName, String outputFileName) throws IOException {
+    	// Uses BaseConverter method to get the input image
     	BufferedImage inputImage = readImage(inputFileName);
         int width = inputImage.getWidth();
         int height = inputImage.getHeight();
-
+        
+        // Creates a new image frame with swapped height and width values
         BufferedImage outputImage = new BufferedImage(height, width, BufferedImage.TYPE_INT_RGB);
-
+        
+        // Loop to iterate through each pixel
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
+            	// Outputs the pixel to its swapped coordinate in the new frame
                 int n = inputImage.getRGB(x, y);
                 outputImage.setRGB(height - 1 - y, x, n);
             }
         }
 
-        // Save the output image instead of displaying it
+      // Uses BaseConverter method to output the image to PixelCraft
         writeImage(outputImage, outputFileName);
     }
 }
-

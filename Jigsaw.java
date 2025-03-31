6b7d06c817d6dd6a485dde8c45e33a132a74fd71
noahdc06 <noahdc06@my.yorku.ca>
2025-03-30 22:48:04 -0400

    Update Jigsaw.java

--- Jigsaw.java
+++ Jigsaw.java
@@ -21,13 +21,13 @@ public class Jigsaw extends BaseConverter {
         writeImage(outputImage, outputFileName);
     }
     
-    //Recursive method to randomly shuffle all tiels.
+    //Recursive method to select which tiles should be shuffled.
     private void shuffleTiles(int tileX, int tileY) {
     	// Base case: The bottom tile is reached, do nothing
         if (tileY >= height) return;
         
-        // Recursively calls itself, iterating through each tile column and through each row
         if (tileX >= width) {
+        	// End of column reached/exceeded: calls itself, moving to the next row of tiles
             shuffleTiles(0, tileY + 50);
         } else {
             //Selects another tile randomly to 'swap positions' with
@@ -35,6 +35,7 @@ public class Jigsaw extends BaseConverter {
             int swapY = (int)(Math.random() * (height/50)) * 50;
             // Begins recursive swapping of each pixel
             swapPixels(tileX, tileY, swapX, swapY, 0, 0);
+            //Calls itself, iterating through each tile column
             shuffleTiles(tileX + 50, tileY);
         }
     }
@@ -42,9 +43,12 @@ public class Jigsaw extends BaseConverter {
     //Recursive method to swap the individual pixels of the selected tiles.
     private void swapPixels(int x1, int y1, int x2, int y2, int dx, int dy) {
     	//Base case: The bottom pixel is reached, do nothing
-        if (dy >= 50) return;
-        // Recursively calls itself for 50 pixels, iterating through each pixel column and through each row
+        if (dy >= 50) {
+        	return;
+        }
+
         if (dx >= 50) {
+        	// End of column reached/exceeded: calls itself, moving to the next row of pixels
             swapPixels(x1, y1, x2, y2, 0, dy + 1);
         } else {
         	// Gets the pixel coordinates of the first tile and the tile it is swapping with
@@ -52,13 +56,14 @@ public class Jigsaw extends BaseConverter {
             int py1 = y1 + dy;
             int px2 = x2 + dx;
             int py2 = y2 + dy;
-            //Checks to ensure the pixel is within bounds, otherwise do nothing
+            //Checks to ensure the pixel is within bounds, otherwise call next immediately
             if (px1 < width && py1 < height && px2 < width && py2 < height) {
             	//Swaps the pixels between the two tiles, using temp to hold the first pixel during the swap
                 int temp = inputImage.getRGB(px1, py1);
                 outputImage.setRGB(px1, py1, inputImage.getRGB(px2, py2));
                 outputImage.setRGB(px2, py2, temp);
             }
+            // Calls itself, iterating through each pixel column
             swapPixels(x1, y1, x2, y2, dx + 1, dy);
         }
     }

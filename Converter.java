e43c1263f64aa1cb8305eacc084679bf30a5f179
noahdc06 <noahdc06@my.yorku.ca>
2025-03-30 20:44:38 -0400

    Update Converter.java

--- Converter.java
+++ Converter.java
@@ -1,7 +1,8 @@
 import java.io.IOException;
 /* File's initial purpose was to be implemented in every converter to allow PixelCraft
  * to run without adding if statements for each converter, which would break the
- * requirement that the PixelCraft main file not be changed.*/
+ * requirement that the PixelCraft main file not be changed. For example, GrayScale
+ * would be "public class GrayScale implements Converter {" */
 
 public interface Converter {
 /* Once we tried using inheritance, we only needed to implement this into the

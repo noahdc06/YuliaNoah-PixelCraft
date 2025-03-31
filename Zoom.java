f4ced01569f0f0e9ce16af4e08088a9065b3a923
noahdc06 <noahdc06@my.yorku.ca>
2025-03-30 23:13:01 -0400

    Update Zoom.java

--- Zoom.java
+++ Zoom.java
@@ -1,44 +1,43 @@
-
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 
+/* This program aims to create a new, 'zoomed' image using the same height and 
+ * width of the original image but increasing the size of each pixel by double */
 public class Zoom extends BaseConverter {
-	
-	@Override
 	public void convert(String inputFileName, String outputFileName) throws IOException {
-		
+		// Uses BaseConverter method to get the input image
 		BufferedImage inputImage = readImage(inputFileName);
 		int originalWidth = inputImage.getWidth();
 		int originalHeight = inputImage.getHeight();
 		
-		// Adjust zoom amount
-		double zoomAmt = 2.0;
-		
-		int newWidth = (int)(originalWidth/zoomAmt);
-		int newHeight = (int)(originalHeight/zoomAmt);
-		
+		//Sets parameters for zoomed image
+		int newWidth = (int)(originalWidth/2.0);
+		int newHeight = (int)(originalHeight/2.0);
 		int startX = (originalWidth-newWidth) / 2;
 		int startY = (originalHeight-newHeight) / 2;
 		
+		 // Creates the frame for the output image
 		BufferedImage outputImage = new BufferedImage(originalWidth, originalHeight, BufferedImage.TYPE_INT_RGB);
 		
+		// Loop to iterate through each pixel
 		for(int y=0; y<originalHeight; y++) {
 			for(int x=0; x<originalWidth; x++) {
-				int originalX = (int)(startX+ (x/zoomAmt));
-				int originalY = (int)(startY+ (y/zoomAmt));
-				
+				//Calculates coordinates of the zoomed pixel
+				int originalX = (int)(startX+ (x/2.0));
+				int originalY = (int)(startY+ (y/2.0));
+				// Ensures coordinates are within the image bounds
 				originalX = Math.max(0,  Math.min(originalX, originalWidth-1));
 				originalY = Math.max(0, Math.min(originalY,  originalHeight-1));
-				
+				// Replaces the pixel in the new image
 				int pixel = inputImage.getRGB(originalX, originalY);
 				outputImage.setRGB(x,  y,  pixel);
 			}
 		}
 		
+		// Uses BaseConverter method to output the image to PixelCraft
 		writeImage(outputImage, outputFileName);
-		
 	}
 
 }

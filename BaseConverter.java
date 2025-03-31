6490c03028975a9b3f89595ea1992fb7a51483a0
noahdc06 <noahdc06@my.yorku.ca>
2025-03-30 20:43:57 -0400

    Update BaseConverter.java

--- BaseConverter.java
+++ BaseConverter.java
@@ -3,13 +3,16 @@ import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 
+/* We used this abstract class to replace the ImageIO read and write 
+ * functionalities which were the same in every converter, to more easily
+ * manage image files, implementing Converter */
 public abstract class BaseConverter implements Converter {
-
-    protected BufferedImage readImage(String filename) throws IOException {
+	// Simple method to read the input image, used in every class
+    public BufferedImage readImage(String filename) throws IOException {
         return ImageIO.read(new File(filename));
     }
-
-    protected void writeImage(BufferedImage image, String filename) throws IOException {
+    // Simple method to write the converted image, used in every class
+    public void writeImage(BufferedImage image, String filename) throws IOException {
         ImageIO.write(image, "png", new File(filename));
     }
 }
